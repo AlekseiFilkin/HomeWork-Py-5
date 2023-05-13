a2029c2f6f1322655a96db2acf6bd3bc2df093ab
Aleksey_Filkin <acid_wind@mail.ru>
2023-05-13 10:37:15 +0700

    Task1.py solved

---
+++ Task1.py
@@ -0,0 +1,16 @@
+'''
+Задача 26:  Напишите программу, которая на вход принимает два числа A и B, и возводит число А в целую степень B с помощью рекурсии.
+
+*Пример:*
+
+A = 3; B = 5 -> 243 (3⁵)
+    A = 2; B = 3 -> 8
+'''
+
+def degrNumbers(a, b):
+    if b == 0:
+        return 1
+    return a * degrNumbers(a, b - 1)
+number = int(input('Введите число: '))
+degree = int(input('Введите степень: '))
+print(f'{number}^{degree} = {degrNumbers(number, degree)}')
